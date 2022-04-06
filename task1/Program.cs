e22000bbad7310b05ca04f1a13f07337e2005424
Nikita <nikitya39@gmail.com>
2022-04-07 00:23:36 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] array = new double [n,m];
+//Random r = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i,j] = new Random().NextDouble();
+         Console.Write($"{array[i,j]}");
+         Console.WriteLine();
+    }
+    Console.WriteLine();
+}
